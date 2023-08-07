b4facffa25defc6cb92ab56157aaaa45e3ce0726
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-08-06 21:57:12 -0400

    Mole Slot use Effect timer

--- src/components/Mole.js
+++ src/components/Mole.js
@@ -1,8 +1,19 @@
 import { useEffect } from "react"
+import moleImg from '../mole.png'
 
-function Mole() {
+function Mole(props) {
+  useEffect(() => {
+    let randomSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randomSeconds)
+        return () => clearTimeout(timer)
+  })
+
   return (
-    <div>Mole</div>
+    <div>
+      <img style={{'width': '30vw'}} src={moleImg} onClick={props.handleBopping} />
+    </div>
   )
 }
 

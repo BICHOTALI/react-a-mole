91c0f4dda2ad980e3d85e7137093108e89e4d4b6
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-08-06 21:56:55 -0400

    useEffect and building a Mole Timer; Empty Slot

--- src/components/EmptySlot.js
+++ src/components/EmptySlot.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
+import MoleHill from '../molehill.png'
 
-function EmptySlot() {
+function EmptySlot(props) {
+    
+    useEffect(() => {
+        let randomSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.toggle(true)
+        }, randomSeconds)
+        return () => clearTimeout(timer)
+    })
+
   return (
-    <div>EmptySlot</div>
+    <div>
+        <img style={{'width': '30vw'}} src={MoleHill} />
+    </div>
   )
 }
 
